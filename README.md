1f74d4e8a6209464920700a0be0378cfa88ee8e2
Joe <jguss45@gmail.com>
2023-03-16 20:45:31 +0100

    added context logic and product details

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This is a [Next.js] project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+The main goal of this project was to practice building a modern, fully responsive, full stack application.
+
+Some bonus things to be learned along the way include:
+
+Modern design, animation, content management (in this case using Sanity), cart functionality, and integration with Stripe
+
+
 ## Getting Started
 
 First, run the development server:
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShopping } from 'react-icons/ai';
+
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -10,12 +14,14 @@ const Navbar = () => {
           Joe's Headphones
         </Link>
       </p>
-      <button type="button" className='cart-icon' onClick="">
+      <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
         <span className='cart-item-qty'>
-          1
+          {totalQuantities}
         </span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
